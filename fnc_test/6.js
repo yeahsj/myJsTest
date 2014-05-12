1ee7813538ff186b288a738e840cace9a4d280bb
sangjun <yeahsj@gmail.com>
2014-05-12 16:12:41 +0800

    add test

---
+++ fnc_test/6.js
@@ -0,0 +1,22 @@
+
+/**
+ * 函数getName在prototype指定.
+ * @param {} name
+ */
+var user = function(name){
+	return new user.fn.init(name);
+};
+
+user.fn=user.prototype={
+	constructor : user,
+	init: function(name){
+		this.name = name;
+//		return this;
+	},
+	getName: function(){
+		return this.name;
+	}
+};
+
+user.fn.init.prototype = user.fn;
+
